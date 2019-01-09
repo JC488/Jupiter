733ff58e1e84e6909481cf9b32a2e60512c684d5
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-01-09 15:59:07 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    ADMINS: ["240508683455299584"],
+    ADMINS: [],
     PREFIX: "!",
-    OWNER_ID: "240508683455299584",
-    BOT_ID: "531502637888045057",
-    BOT_TOKEN: "NTMxNTAyNjM3ODg4MDQ1MDU3.DxO4KQ.krQMUoH2TyDyK1txyCVU7a2zInw",
+    OWNER_ID: "",
+    BOT_ID: "",
+    BOT_TOKEN: "",
     MUSIC_OPTS: {
         part: "snippet",
         maxResults: 5,
-        key: "AIzaSyAkC9g9W5CSarCaLVJqDNkQ0n_U6tldxIk",
+        key: "",
         type: "video",
         videoCategoryId: 10
     }
