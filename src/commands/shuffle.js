5a6bebf3c87777109e07d17d2296cd490aa66267
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-01-11 15:04:13 +0100

    Update shuffle.js

--- src/commands/shuffle.js
+++ src/commands/shuffle.js
@@ -17,13 +17,13 @@ class Shuffle extends Command {
     if(!message.guild.voiceConnection) {
       return this.client.music.sendEmbed(message, "⚠ Je ne suis pas connecté dans un salon-vocal !");
     }
-    if(!message.member.voiceChannel) {
+    else if(!message.member.voiceChannel) {
       return this.client.music.sendEmbed(message, "⚠ Vous devez être connecté dans un salon-vocal !");
     }
-    if(!message.member.voiceChannel.speakable) {
+    else if(!message.member.voiceChannel.speakable) {
       return this.client.music.sendEmbed(message, "⚠ Je n'ai pas la permission de `rejoindre` ou `parler` dans ce salon !");
     }
-    if(!message.guild.voiceConnection.player.dispatcher || message.guild.voiceConnection.player.dispatcher.paused) {
+    else if(!message.guild.voiceConnection.player.dispatcher || message.guild.voiceConnection.player.dispatcher.paused) {
       return this.client.music.sendEmbed(message, ":point_up::skin-tone-3: Je ne joue actuellement pas.");
     }
         let queue = this.client.music.getQueue(message.guild.id);
