ad45edcea9cfcf0cf5dbd93fdea4dc43d69edce0
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-01-11 14:57:58 +0100

    new command: moveto

---
+++ src/commands/moveto.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const Command = require("../structures/Command");
+
+class Moveto extends Command {
+  constructor(client) {
+    super(client, {
+      name: "moveto",
+      description: "Changer le bot de salon vocal",
+      category: "Musique",
+      usage: "moveto <ID du salon>",
+      aliases: []
+    });
+  }
+
+  async run(message, args) {
+    let client = this.client;
+    let search = args.slice(1).join(" ");
+    if(!search) {
+        return client.music.sendEmbed(message, "⚠ Donnez-moi l'ID d'un salon vocal !");
+    }
+        await client.music.moveTo(message, search);
+  }
+}
+
+module.exports = Moveto;
