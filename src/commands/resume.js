41624a82dfdfd7ba2dd671f297878e40ade19c1f
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-01-11 15:03:54 +0100

    Update resume.js

--- src/commands/resume.js
+++ src/commands/resume.js
@@ -18,13 +18,13 @@ class Resume extends Command {
     if(!message.guild.voiceConnection) {
       return client.music.sendEmbed(message, "⚠ Je ne suis pas connecté dans un salon-vocal !");
     }
-    if(!message.member.voiceChannel) {
+    else if(!message.member.voiceChannel) {
       return client.music.sendEmbed(message, "⚠ Vous devez être connecté dans un salon-vocal !");
     }
-    if(!message.member.voiceChannel.speakable) {
+    else if(!message.member.voiceChannel.speakable) {
       return client.music.sendEmbed(message, "⚠ Je n'ai pas la permission de `rejoindre` ou `parler` dans ce salon !");
     }
-    if(!message.guild.voiceConnection.player.dispatcher) {
+    else if(!message.guild.voiceConnection.player.dispatcher) {
       return client.music.sendEmbed(message, ":point_up::skin-tone-3: Je n'ai pas de musique à jouer !");
     }
       if(message.guild.voiceConnection.player.dispatcher.paused) {
