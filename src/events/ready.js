e788a68c757930a9abb886d8f3923595f395b608
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-01-15 00:55:59 +0100

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,5 +1,5 @@
 const Event = require("../structures/Event");
-const logo = require("asciiart-logo");
+const logo = require('asciiart-logo');
 
 class Ready extends Event {
   constructor(args) {
@@ -9,16 +9,17 @@ class Ready extends Event {
   }
 
   async run() {
-    if(!this.client.user.bot) {
+    let client = this.client;
+    if(!client.user.bot) {
       return process.exit(0);
     }
 
-    await this.client.user.setPresence({game:{name: `${this.client.config.PREFIX}help`}});
+    await client.user.setPresence({game:{name: `${client.config.PREFIX}help`}});
     await new Promise((resolve) => setTimeout(resolve, 1000));
-    console.log(
+    await console.log(
       logo({
-        name: `${this.client.user.username}`,
-        font: "Speed",
+        name: `${client.user.username}`,
+        font: 'Speed',
         lineChars: 15,
         padding: 5,
         margin: 2
@@ -26,7 +27,7 @@ class Ready extends Event {
       .emptyLine()
       .right(`version ${require("../../package").version}`)
       .emptyLine()
-      .wrap(`${this.client.user.username}#${this.client.user.discriminator} développé par Sworder.`)
+      .wrap(`${client.user.username}#${client.user.discriminator} développé par Sworder.`)
       .render()
     );
     client.updater.updateStats();
