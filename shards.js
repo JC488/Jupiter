7b9209d4d77d43602a8eef9dd048bacacfc0313f
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-01-15 01:05:51 +0100

    Update shards.js

--- shards.js
+++ shards.js
@@ -3,13 +3,16 @@ const config = require("./config");
 const LOGGER = require("./src/utils/helpers/logger");
 const logger = new LOGGER();
 
-const shards = new ShardingManager("./index.js", {
+const sharder = new ShardingManager("./index.js", {
     token: config.BOT_TOKEN,
     totalShards: "auto"
 });
 
-shards.on("launch", (shard) => {
-    logger.log(`[SHARDING] Shard #${shard.id} lancé`);
+sharder.on("launch", (shard) => {
+    logger.log(`[SHARDING] Shard #${shard.id} lancé !`);
 });
 
-shards.spawn();
+sharder.spawn()
+    .then(() => {
+        logger.log("[SHARDING] Tous les shards sont lancés !");
+    });
