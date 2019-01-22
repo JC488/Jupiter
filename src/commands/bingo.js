cb914b52a3a2f9b26cbc8ee7cc825860ef4fd349
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-01-22 11:58:23 +0100

    limit < -1000 || limit > 1000

--- src/commands/bingo.js
+++ src/commands/bingo.js
@@ -27,45 +27,48 @@ class Bingo extends Command {
     else if(limit > 1000) {
         return message.channel.send("⚠ Ce n'est pas une limite valide, veuillez taper un nombre < **1000**!");
     }
+    else if(limit < -1000) {
+        return message.channel.send("⚠ Ce n'est pas une limite valide, veuillez taper un nombre > **-1000**!");
+    }
     
-    message.channel.send(`Que le bingo commence ! Vous avez **1** minute pour trouver un nombre compris entre **0** et **${Math.round(limit)}**`).then(async(m) => {
-        const random = Math.floor(Math.random() * limit);
-        const filter = m => m.author.id !== client.user.id;
-        const collector = await m.channel.createMessageCollector(filter, { time: 60000 });
+      message.channel.send(`Que le bingo commence ! Vous avez **1** minute pour trouver un nombre compris entre **0** et **${Math.round(limit)}**`).then(async(m) => {
+          const random = Math.floor(Math.random() * limit);
+          const filter = m => m.author.id !== client.user.id;
+          const collector = await m.channel.createMessageCollector(filter, { time: 60000 });
 
-        client.cooldown.bingo[m.guild.id] = true;
+          client.cooldown.bingo[m.guild.id] = true;
 
-        collector.on("collect", async(collected) => {
-            if(collected.content.toLowerCase() === "annuler") {
-                return collector.stop(`✅ Bingo annulé !`);
-            } else {
-                let response = await collected.content.trim();
-                response = parseInt(response);
+          collector.on("collect", async(collected) => {
+              if(collected.content.toLowerCase() === "annuler") {
+                  return collector.stop(`✅ Bingo annulé !`);
+              } else {
+                  let response = await collected.content.trim();
+                  response = parseInt(response);
 
-                if(isNaN(response)) {
-                    return message.channel.send("⚠ Ce n'est pas un nombre !");
-                }
-                else if(response < random) {
-                    return message.channel.send("⚠ C'est plus !");
-                }
-                else if(response > random) {
-                    return message.channel.send("⚠ C'est moins !");
-                }
-                else if(response === random) {
-                    await collector.stop(`${collected.author.toString()} a remporté le Bingo, le nombre était: **${random}**`);
-                }
-            }
-        });
-        collector.on("end", async(collected, reason) => {
-            delete client.cooldown.bingo[m.guild.id];
+                  if(isNaN(response)) {
+                      return message.channel.send("⚠ Ce n'est pas un nombre !");
+                  }
+                  else if(response < random) {
+                      return message.channel.send("⚠ C'est plus !");
+                  }
+                  else if(response > random) {
+                      return message.channel.send("⚠ C'est moins !");
+                  }
+                  else if(response === random) {
+                      await collector.stop(`${collected.author.toString()} a remporté le Bingo, le nombre était: **${random}**`);
+                  }
+              }
+          });
+          collector.on("end", async(collected, reason) => {
+              delete client.cooldown.bingo[m.guild.id];
 
-            if(reason && reason !== "time") {
-                return message.channel.send(reason);
-            } else {
-                return message.channel.send(`Personne a remporté le Bingo, le nombre était: **${random}**`);
-            }
-        });
-    });
+              if(reason && reason !== "time") {
+                  return message.channel.send(reason);
+              } else {
+                  return message.channel.send(`Personne a remporté le Bingo, le nombre était: **${random}**`);
+              }
+          });
+      });
   }
 }
 
