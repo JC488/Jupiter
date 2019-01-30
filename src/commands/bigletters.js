42254cd00cbdc4aa16112e0625db9499a5b0e216
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-01-30 13:02:09 +0100

    new command: bigletters

---
+++ src/commands/bigletters.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const Command = require("../structures/Command");
+
+class Bigletters extends Command {
+  constructor(client) {
+    super(client, {
+      name: "bigletters",
+      description: "Rendre vos mots plus grands !",
+      category: "Fun",
+      usage: "bigletters <texte>",
+      aliases: []
+    });
+}
+
+  async run(message, args) {
+    let client = this.client;
+    let text = args.slice(1);
+    
+    if(!text) {
+        return client.music.sendEmbed(message, "⚠ Aucun texte spécifié !");
+    }
+
+        text = text.join(" ")
+            .replace(/[^a-zA-Z ]/g, "");
+
+        let letterArr = text.split("")
+            .map((w) => (w === " " ? " " : `:regional_indicator_${w}:`))
+            .join("");
+
+        message.channel.send(letterArr)
+            .catch((err) => {
+                if(err) {
+                    return client.music.sendEmbed(message, "❌ Il y a trop de caractères ! (Max. 2000)");
+                }
+            });
+  }
+}
+
+module.exports = Bigletters;
