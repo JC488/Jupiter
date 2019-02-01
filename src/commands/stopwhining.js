ff524eb3676da4d3566d2cf584778b004426076b
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-02-02 00:54:54 +0100

    Add files via upload

---
+++ src/commands/stopwhining.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const Command = require("../structures/Command");
+
+class Stopwhining extends Command {
+  constructor(client) {
+    super(client, {
+      name: "stopwhining",
+      description: "Entendre un son",
+      category: "Soundboard",
+      usage: "stopwhining",
+      aliases: []
+    });
+  }
+
+  async run(message, args) {
+    let client = this.client;
+    if(!message.member.voiceChannel) {
+      return client.music.sendEmbed(message, "⚠ Vous devez être connecté dans un salon-vocal !");
+    }
+    else if(!message.member.voiceChannel.joinable) {
+      return client.music.sendEmbed(message, "⚠ Je n'ai pas la permission de `rejoindre` ou `parler` dans ce salon !");
+    }
+    else if(!message.member.voiceChannel.speakable) {
+      return client.music.sendEmbed(message, "⚠ Je n'ai pas la permission de `rejoindre` ou `parler` dans ce salon !");
+    }
+      message.member.voiceChannel.join()
+        .then(async(connection) => {
+          await connection.sendVoiceStateUpdate({
+            self_deaf: true
+          });
+          await connection.playFile(`./src/assets/soundboard/${this.name}.wav`);
+        })
+          .catch((err) => {
+            if(err) {
+              return client.music.sendEmbed(message, "❌ Une erreur est survenue !");
+            }
+          });
+  }
+}
+
+module.exports = Stopwhining;
